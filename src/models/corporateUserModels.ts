28d18e5c054f40ee2d88b52e8bf3bf2735dc4f4d
vishesh kumar <linuxtovishesh@gmail.com>
2024-06-26 18:49:53 +0530

    domain validation

--- src/models/corporateUserModels.ts
+++ src/models/corporateUserModels.ts
@@ -14,43 +14,62 @@ export interface CorporateUser {
 export const createCorporateUser = async (
   connection: any,
   profile: CorporateUser
-): Promise<CorporateUser> => {
+): Promise<any> => {
   try {
-    const genDate = new Date().toISOString();
-    const [result] = await connection.execute(
-      "INSERT INTO corporateUser (email, firstName, lastName, code, mobileNumber, otp, status, genTs) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-      [
-        profile.email,
-        profile.firstName,
-        profile.lastName,
-        profile.code,
-        profile.mobileNumber,
-        profile.otp,
-        true,
-        genDate,
-      ]
-    );
-    const insertedProfile = { ...profile, id: (result as any).insertId };
-    console.log("inserted profile ", insertedProfile);
-    return insertedProfile;
+    const queryq = `SELECT *
+      FROM profile
+      WHERE code = '${profile.code}'`;
+    const [getProfile] = await connection.execute(queryq);
+    console.log("getProfiel  is  ", getProfile[0]);
+    const domains = getProfile[0].emailDomains;
+    const email = profile.email.split("@")[1];
+    console.log("domain is  ", domains)
+    console.log("email is ", email);
+    const domainList = domains.split(",");
+    const isAllowed = isEmailAllowed(email, domainList);
+    if (isAllowed) {
+      const genDate = new Date().toISOString();
+      const [result] = await connection.execute(
+        "INSERT INTO corporateUser (email, firstName, lastName, code, mobileNumber, otp, status, genTs) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          profile.email,
+          profile.firstName,
+          profile.lastName,
+          profile.code,
+          profile.mobileNumber,
+          profile.otp,
+          true,
+          genDate,
+        ]
+      );
+      const insertedProfile = { ...profile, id: (result as any).insertId };
+      console.log("inserted profile ", insertedProfile);
+      return insertedProfile;
+    } else {
+      const result = { message: "domain not found" };
+      return result;
+    }
   } catch (error) {
     console.error("Error creating corporateUser:", error);
     throw error;
   }
 };
-export const getCorporateUserByCodeModel = async (connection: any,  code: any
+const isEmailAllowed = (emailDomain: any, allowedDomains: any) => {
+  return allowedDomains.some((domain:any) => domain.trim() === emailDomain);
+};
+export const getCorporateUserByCodeModel = async (
+  connection: any,
+  code: any
 ): Promise<CorporateUser> => {
   console.log(code);
   try {
     const [result] = await connection.execute(
       "SELECT * FROM corporateUser WHERE code =?",
-      [
-        code
-      ]
+      [code]
     );
-    return result; 
+    return result;
   } catch (error) {
     console.error("Error creating corporateUser:", error);
     throw error;
   }
-}
+};
